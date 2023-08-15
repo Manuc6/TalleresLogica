f8353d95913db18ea8545b588dc9b00fd1aa9de3
Marcos VE <>
2023-08-15 17:01:03 -0500

    Cambios
    Taller

---
+++ TallerLogica/src/Taller/Ejercicios.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller;
+
+/**
+ *
+ * @author 503
+ */
+public class Ejercicios {
+    public static void mein (String [] args){
+        double a= 3+(8*5)-(6/3);
+        System.out.println("Resultado "+a);
+       
+        /*Ejercicio 2*/
+        double b= (2.5*2*3)-(4/2)+8;
+        System.out.println("Resultado "+b);
+        
+        /*Ejercicio 3*/
+        
+    }
+    
+}
